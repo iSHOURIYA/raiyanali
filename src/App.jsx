763694e05c1573f9740d129bc6feab03fda46367
Shaurya Tayal <shouriyatayal.dev@yahoo.com>
2025-03-20 19:02:16 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,19 @@ import Hyperspeed from "./blocks/Backgrounds/Hyperspeed/Hyperspeed";
 import TrueFocus from "./blocks/TextAnimations/TrueFocus/TrueFocus";
 import "./blocks/Backgrounds/Hyperspeed/Hyperspeed.css";
 
+
+
 function App() {
   return (
-    <div className="relative h-screen w-full overflow-hidden">
-      <Hyperspeed />
-
+    <div id = "main">
+      <div>
+        <Hyperspeed />
+      </div>
+      <div class = "container">
+        <TrueFocus />
+      </div>
     </div>
+
   );
 }
 
