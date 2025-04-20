064cfaaac6c7f14ec6b9e2b7f5f17bfb56052f04
Shaurya Tayal <shouriyatayal.dev@yahoo.com>
2025-04-21 03:46:12 +0530

    Raiyan

--- src/components/ui/Card.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Card = ({ children, className }) => {
-  return <div className={`bg-gray-800 p-6 rounded-lg ${className}`}>{children}</div>;
-};
-
-export const CardContent = ({ children }) => {
-  return <div className="p-4">{children}</div>;
-};
